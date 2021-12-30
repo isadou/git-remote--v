e78c18315e1d49f4a47e47e827fb1c44b86b8cad
isabelledouin <isabelle.douin@orange.fr>
2021-12-30 17:07:27 +0100

    mise en place routes, controller et views sitest

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+  end
+
+  def home
+  end
+
+end
